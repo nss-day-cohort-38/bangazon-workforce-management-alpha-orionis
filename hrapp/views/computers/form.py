e07af881d683941994e9276591eb74d29dead5de
Tyler Simeone <tyler.simeone@gmail.com>
2020-05-11 14:27:31 -0500

    added template to add new computer

--- hrapp/views/computers/form.py
+++ hrapp/views/computers/form.py
@@ -1,9 +1,8 @@
 import sqlite3
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.decorators import login_required
 from ..connection import Connection
 
-@login_required
 def computer_form(request):
     if request.method == 'GET':
         template = 'computers/form.html'
