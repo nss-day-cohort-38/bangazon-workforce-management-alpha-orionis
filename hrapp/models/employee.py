501bb2b41d0053b45f85edd867514f229cd1e6ca
CoopNich <CoopNich92@gmail.com>
2020-05-11 11:29:40 -0500

    employees and their departments display in /employees

--- hrapp/models/employee.py
+++ hrapp/models/employee.py
@@ -8,7 +8,7 @@ class Employee(models.Model):
     last_name = models.CharField(max_length=100)
     start_date = models.DateField()
     is_supervisor = models.BooleanField()
-    # department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = ("Employee")
--- hrapp/views/employees/list.py
+++ hrapp/views/employees/list.py
@@ -9,15 +9,18 @@ def employee_list(request):
             conn.row_factory = sqlite3.Row
             db_cursor = conn.cursor()
 
-            # TODO: Add to query: e.department,
             db_cursor.execute("""
             select
-                e.id,
+                e.id employee_id,
                 e.first_name,
                 e.last_name,
                 e.start_date,
-                e.is_supervisor
+                e.is_supervisor,
+                e.department_id,
+                d.id department_id,
+                d.department_name
             from hrapp_employee e
+            JOIN hrapp_department d ON e.department_id = d.id
             """)
 
             all_employees = []
@@ -25,12 +28,12 @@ def employee_list(request):
 
             for row in dataset:
                 employee = Employee()
-                employee.id = row['id']
+                employee.id = row['employee_id']
                 employee.first_name = row['first_name']
                 employee.last_name = row['last_name']
                 employee.start_date = row['start_date']
                 employee.is_supervisor = row['is_supervisor']
-                # employee.department = row['department']
+                employee.department_id = row['department_id']
 
                 all_employees.append(employee)
 
