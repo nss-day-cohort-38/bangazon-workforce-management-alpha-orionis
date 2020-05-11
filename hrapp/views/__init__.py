53287c28b5f1be9c3072091214ea6a6d81ac9bec
CoopNich <CoopNich92@gmail.com>
2020-05-11 12:07:50 -0500

    package in views fixed for training programs

--- hrapp/views/__init__.py
+++ hrapp/views/__init__.py
@@ -1,6 +1,6 @@
 from .employees.list import employee_list
 from .home import home
 from .auth.logout import logout_user
-from .trainingprograms.list import trainingprograms
+from .trainingprograms.list import training_program_list
 from .computers.list import computer_list
 from .computers.list import computer_list
