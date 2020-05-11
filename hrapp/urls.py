0abf40ec4d70fc783a9308dcc2922c9925411a2a
CoopNich <CoopNich92@gmail.com>
2020-05-11 14:57:18 -0500

    can add employees and assign them a department

--- hrapp/urls.py
+++ hrapp/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', logout_user, name='logout'),
     path('employees/', employee_list, name='employee_list'),
+    path('employee/form', employee_form, name='book_form'),
     path('programs/', training_program_list, name='training'),
     path('computers/', computer_list, name='computer_list'),
 ]
--- hrapp/views/__init__.py
+++ hrapp/views/__init__.py
@@ -1,4 +1,5 @@
 from .employees.list import employee_list
+from .employees.form import employee_form
 from .home import home
 from .auth.logout import logout_user
 from .trainingprograms.list import training_program_list
