8d6745898f241a0067df6ba1c5c757d476cb2455
Bito Mann <bito@bitomann.com>
2020-05-12 16:02:48 -0500

    random commit

--- hrapp/views/departments/details.py
+++ hrapp/views/departments/details.py
@@ -3,11 +3,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from hrapp.models import Department
 from hrapp.models import Employee
+# from hrapp.models import model_factory
 from ..connection import Connection
-from django.contrib.auth.decorators import login_required
 
 def get_department(department_id):
     with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = create_department
         db_cursor = conn.cursor()
 
         db_cursor.execute("""
@@ -20,18 +21,38 @@ def get_department(department_id):
         e.id	
         FROM hrapp_department d
         LEFT JOIN hrapp_employee e
-        ON e.id = d.id
-        """, ( ))
+        ON e.department_id = d.id
+        WHERE d.id = ?
+        """, (department_id,))
 
-        department = db_cursor.fetchone()
-        return department
+        return db_cursor.fetchone()
+
+def create_department(cursor, row):
+    _row = sqlite3.Row(cursor, row)
+
+    department = Department()
+    department.id = _row["id"]
+    department.name = _row["department_name"]
+    department.budget = _row["budget"]
+
+    employee = Employee()
+    employee.id = _row["id"]
+    employee.first_name = _row["first_name"]
+    employee.last_name = _row["last_name"]
+    # employee.start_date = _row["start_date"]
+    # employee.is_supervisor = _row["is_supervisor"]
+
+    department.employee = employee
+
+    return department
+
 
 @login_required
 def department_details(request, department_id):
     if request.method == 'GET':
         department = get_department(department_id)
 
-        template = 'departments/department_details.html'
+        template = 'departments/details.html'
         context = {
             'department': department
         }
