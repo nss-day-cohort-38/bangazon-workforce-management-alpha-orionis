07eb73e679e0a32727085607a3617e4d37ad6a29
Tyler Simeone <tyler.simeone@gmail.com>
2020-05-12 08:52:13 -0500

    detail view for computer added, now working on template

--- hrapp/views/computers/details.py
+++ hrapp/views/computers/details.py
@@ -2,4 +2,43 @@ import sqlite3
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from hrapp.models import Computer
 from ..connection import Connection
+
+
+def get_computer(computer_id):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = create_computer
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            c.id,
+            c.make,
+            c.purchase_date,
+            c.manufacturer
+        FROM hrapp_computer c
+        WHERE c.id = ?
+        """, (computer_id,))
+
+        return db_cursor.fetchone()
+
+def create_computer(cursor, row):
+    _row = sqlite3.Row(cursor, row)
+
+    computer = Computer()
+    computer.id = _row['id']
+    computer.make = _row['make']
+    computer.purchase_date = _row['purchase_date']
+    computer.manufacturer = _row['manufacturer']
+
+def computer_details(request, computer_id):
+    if request.method == 'GET':
+        computer = get_computer(computer_id)
+
+        template = 'computers/details.html'
+        context = {
+            'computer': computer
+        }
+
+        return render(request, template, context)
